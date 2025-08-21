7eaa8bb3a0fb35f5b7ca2b43f3c32dfa58bd900b
FirozKamdar <firozkamdar01@gmail.com>
2025-08-21 13:38:57 +0530

    minor ui improvement

--- src/app/(user)/posts/create/page.tsx
+++ src/app/(user)/posts/create/page.tsx
@@ -180,8 +180,8 @@ export default function CreatePostPage() {
         </Select>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-        <div className="lg:col-span-3 flex flex-col gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+        <div className="lg:col-span-2 flex flex-col gap-6">
           <AccountsCard
             accounts={brandAccounts}
             selectedAccounts={selectedAccounts}
