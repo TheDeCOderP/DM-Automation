cc70e3d6f57c92af57578472f5e23bc22dcdb495
FirozKamdar <firozkamdar01@gmail.com>
2025-09-23 18:13:19 +0530

    minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
 
   if (session) {
-    redirect('/posts/calendar');
+    //redirect('/posts/calendar');
   }
 
   return (
